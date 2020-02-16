7cf0f24c7e770ac3014725e8c93c3658bc7f90d0
Jorge Luis Calderín García <JorgeCal@youforce.net>
2020-02-16 20:36:07 +0100

    Add project files.

---
+++ HealthChecjsUIClient/MyWebFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthChecjsUIClientTests
+{
+    public class MyWebFactory<T> : WebApplicationFactory<T> where T : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddControllers(c =>
+                {
+                    
+                });
+            });
+        }
+    }
+}
---
+++ HealthChecjsUIClient/TestConcurrencyIssues.cs
@@ -0,0 +1,84 @@
+using FluentAssertions;
+using HealthChecksUIClient;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HealthChecjsUIClientTests
+{
+    public class TestConcurrencyIssues : IClassFixture<MyWebFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> factory;
+
+        public TestConcurrencyIssues(
+            MyWebFactory<Startup> factory
+        ) {
+            this.factory = factory.WithWebHostBuilder(builder => {
+                
+            });
+        }
+
+        [Fact]
+        public async Task TestConcurrency_1()
+        {
+            // Arrange
+            var client = factory.CreateClient();
+
+            // Act
+            var result = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task TestConcurrency_2()
+        {
+            // Arrange
+            var client = factory.CreateClient();
+
+            // Act
+            var result = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task TestConcurrency_3()
+        {
+            // Arrange
+            var client = factory.CreateClient();
+
+            // Act
+            var result = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public Task TestConcurrency_4()
+        {
+            return TestConcurrency_1();
+        }
+        [Fact]
+        public Task TestConcurrency_5()
+        {
+            return TestConcurrency_1();
+        }
+        [Fact]
+        public Task TestConcurrency_6()
+        {
+            return TestConcurrency_1();
+        }
+        [Fact]
+        public Task TestConcurrency_7()
+        {
+            return TestConcurrency_1();
+        }
+
+    }
+}
